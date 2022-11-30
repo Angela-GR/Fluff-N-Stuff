686c1ac737f0642540ce4f8baca827636f949c12
Student <dgoad22@basecampcodingacademy.org>
2022-11-30 15:16:32 -0600

    HOme page, urls, models, and views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
+
+#This page is to link our admin page info to our html pages.
+
+#Opening Page
+def home(request):
+    return render(request,"accounts/index.html")
+
+#Dashboard Page(where we can see customer's info)
+
+#Products Page
+def products(request):
+    return render(request, "accounts/products.html", {"products":products})
+
+#Customers Page
+def customers(request):
+    return render(request, "accounts/customers.html")
